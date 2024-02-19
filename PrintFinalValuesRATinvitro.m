c08285a1a0a506cba8e92ca9903fa7bf867fb31c
lmbilinsky <157535868+lmbilinsky@users.noreply.github.com>
2024-02-18 21:27:34 -0500

    Add files via upload

--- PrintFinalValuesRATinvitro.m
+++ PrintFinalValuesRATinvitro.m
@@ -96,5 +96,5 @@ fprintf(fid, ' \t\t\t\t\t\t\t\t TOTin=%4.2f   \n\n\n',mitoGSSGinliver);
 
 
 fclose(fid);
-open Steadystate.txt;   %this opens a window with the info in it
+open FinalState.txt;   %this opens a window with the info in it
   
